393359c3b0a9425742cc336707d8c15496156b64
taniya221 <taniya.jaison007@gmail.com>
2026-07-13 22:39:37 +0530

    updated

--- src/components/AddCar.jsx
+++ src/components/AddCar.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavigationBar from './NavigationBar'
 
 const AddCar = () => {
      const [input, changeInput] = useState(
@@ -43,6 +44,7 @@ const AddCar = () => {
     
         
       <div>
+        <NavigationBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
