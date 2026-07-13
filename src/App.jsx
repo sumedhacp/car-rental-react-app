cf0e35273161e3f36e72779bae0ccc137882bbad
taniya221 <taniya.jaison007@gmail.com>
2026-07-13 22:32:41 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import AddCar from './components/AddCar'
+
 
 function App() {
   const [count, setCount] = useState(0)
