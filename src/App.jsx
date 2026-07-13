67dae8594aa02b38a5c832ccd0efc2d6964b6eb7
taniya221 <taniya.jaison007@gmail.com>
2026-07-13 22:25:59 +0530

    feature/addCar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddCar from './components/AddCar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+   <AddCar />
     </>
   )
 }
