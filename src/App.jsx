f7c6026f99830fdaa9a103fc67ebb094b80427bf
annetpault <annetmca25@gmail.com>
2026-07-13 22:32:33 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import ViewBar from './components/ViewBar'
+
 
 function App() {
   const [count, setCount] = useState(0)
